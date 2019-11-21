e7cfc5ec5c9bced74d5e9a9298f189f21d86c6da
daniel <bumbles0506@gmail.com>
2019-11-21 15:53:13 +0800

    Friend & Message

---
+++ mad2week4/FriendController.swift
@@ -0,0 +1,37 @@
+//
+//  FriendController.swift
+//  mad2week4
+//
+//  Created by MAD2_P03 on 21/11/19.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+
+
+class FriendController{
+    
+    
+    func AddFriend(friend: Friend)
+    {
+        
+        
+    }
+    
+    
+    func AddMessageToFriend(friend:Friend, message:Message)
+    {
+        
+    }
+    
+    func retrieveMessagebyFriend(friend:Friend)->[Message]
+    {
+        
+    }
+    
+    
+    
+}
