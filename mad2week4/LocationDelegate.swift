bd72a64a2dca6bb3500ae485296761b598148a41
DxnielRavindran <bumbles0506@gmail.com>
2020-01-07 15:36:13 +0800

    .

---
+++ mad2week4/LocationDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  LocationDelegate.swift
+//  mad2week4
+//
+//  Created by Daniel Ravindran on 7/1/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+
+class LocationDelegate : NSObject, CLLocationManagerDelegate {
+    
+    var locationCallback: ((CLLocation) -> ())? = nil; func locationManager(_ manager: CLLocationManager,
+                                                                            
+didUpdateLocations locations: [CLLocation])
+{
+    guard let currentLocation = locations.last else { return }; locationCallback?(currentLocation)
+}
+    
+}
---
+++ mad2week4/MapViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MapViewController.swift
+//  mad2week4
+//
+//  Created by Daniel Ravindran on 7/1/20.
+//  Copyright © 2020 Daniel. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    
+    @IBOutlet weak var MapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+   
+    }
+
+    
+}
+
+
