9fcf0065a126947b2cc8b3304aa6d512111308b6
DxnielRavindran <bumbles0506@gmail.com>
2020-01-12 11:57:11 +0800

    done

--- mad2week4/LocationDelegate.swift
+++ mad2week4/LocationDelegate.swift
@@ -11,12 +11,13 @@ import CoreLocation
 
 
 class LocationDelegate : NSObject, CLLocationManagerDelegate {
-    
-    var locationCallback: ((CLLocation) -> ())? = nil; func locationManager(_ manager: CLLocationManager,
-                                                                            
-didUpdateLocations locations: [CLLocation])
-{
-    guard let currentLocation = locations.last else { return }; locationCallback?(currentLocation)
-}
-    
+
+    var locationCallback: ((CLLocation) -> ())? = nil
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+    guard let currentLocation = locations.last else { return }
+    locationCallback?(currentLocation)
+    }
+
 }
